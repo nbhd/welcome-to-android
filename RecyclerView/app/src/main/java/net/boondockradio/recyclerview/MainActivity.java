7eb87fdb3d295893cf4c5582b65d452dbbf40809
nbhd <boondockradio@gmail.com>
2016-06-14 14:02:55 +0900

    recycler view v2

--- RecyclerView/app/src/main/java/net/boondockradio/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/net/boondockradio/recyclerview/MainActivity.java
@@ -1,5 +1,7 @@
 package net.boondockradio.recyclerview;
 
+import net.boondockradio.recyclerview.dto.StateItem;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,7 +11,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> states;
+    private ArrayList<StateItem> states;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
@@ -31,55 +33,51 @@ public class MainActivity extends AppCompatActivity {
 
     private void createDummyData() {
         states = new ArrayList();
-        states.add("Alabama");
-        states.add("Alaska");
-        states.add("Arizona");
-        states.add("Arkansas");
-        states.add("California");
-        states.add("Colorado");
-        states.add("Connecticut");
-        states.add("Delaware");
-        states.add("Florida");
-        states.add("Georgia");
-        states.add("Hawaii");
-        states.add("Idaho");
-        states.add("Illinois");
-        states.add("Indiana");
-        states.add("Iowa");
-        states.add("Kansas");
-        states.add("Kentucky");
-        states.add("Louisiana");
-        states.add("Maine");
-        states.add("Maryland");
-        states.add("Massachusetts");
-        states.add("Michigan");
-        states.add("Minnesota");
-        states.add("Mississippi");
-        states.add("Missouri");
-        states.add("Montana");
-        states.add("Nebraska");
-        states.add("Nevada");
-        states.add("New Hampshire");
-        states.add("New Jersey");
-        states.add("New Mexico");
-        states.add("New York");
-        states.add("North Carolina");
-        states.add("North Dakota");
-        states.add("Ohio");
-        states.add("Oklahoma");
-        states.add("Oregon");
-        states.add("Pennsylvania ");
-        states.add("Rhode Island");
-        states.add("South Carolina");
-        states.add("South Dakota");
-        states.add("Tennessee");
-        states.add("Texas");
-        states.add("Utah");
-        states.add("Vermont");
-        states.add("Virginia");
-        states.add("Washington");
-        states.add("West Virginia");
-        states.add("Wisconsin");
-        states.add("Wyoming");
+        states.add(new StateItem("Alabama"));
+        states.add(new StateItem("Alaska"));
+        states.add(new StateItem("Arizona"));
+        states.add(new StateItem("Arkansas"));
+        states.add(new StateItem("California"));
+        states.add(new StateItem("Georgia"));
+        states.add(new StateItem("Hawaii"));
+        states.add(new StateItem("Idaho"));
+        states.add(new StateItem("Illinois"));
+        states.add(new StateItem("Indiana"));
+        states.add(new StateItem("Iowa"));
+        states.add(new StateItem("Kansas"));
+        states.add(new StateItem("Kentucky"));
+        states.add(new StateItem("Louisiana"));
+        states.add(new StateItem("Maine"));
+        states.add(new StateItem("Maryland"));
+        states.add(new StateItem("Massachusetts"));
+        states.add(new StateItem("Michigan"));
+        states.add(new StateItem("Minnesota"));
+        states.add(new StateItem("Mississippi"));
+        states.add(new StateItem("Missouri"));
+        states.add(new StateItem("Montana"));
+        states.add(new StateItem("Nebraska"));
+        states.add(new StateItem("Nevada"));
+        states.add(new StateItem("New Hampshire"));
+        states.add(new StateItem("New Jersey"));
+        states.add(new StateItem("New Mexico"));
+        states.add(new StateItem("New York"));
+        states.add(new StateItem("North Carolina"));
+        states.add(new StateItem("North Dakota"));
+        states.add(new StateItem("Ohio"));
+        states.add(new StateItem("Oklahoma"));
+        states.add(new StateItem("Oregon"));
+        states.add(new StateItem("Pennsylvania "));
+        states.add(new StateItem("Rhode Island"));
+        states.add(new StateItem("South Carolina"));
+        states.add(new StateItem("South Dakota"));
+        states.add(new StateItem("Tennessee"));
+        states.add(new StateItem("Texas"));
+        states.add(new StateItem("Utah"));
+        states.add(new StateItem("Vermont"));
+        states.add(new StateItem("Virginia"));
+        states.add(new StateItem("Washington"));
+        states.add(new StateItem("West Virginia"));
+        states.add(new StateItem("Wisconsin"));
+        states.add(new StateItem("Wyoming"));
     }
 }
---
+++ RecyclerView/app/src/main/java/net/boondockradio/recyclerview/dto/StateItem.java
@@ -0,0 +1,10 @@
+package net.boondockradio.recyclerview.dto;
+
+public class StateItem {
+
+    public String title;
+
+    public StateItem(String title) {
+        this.title = title;
+    }
+}
